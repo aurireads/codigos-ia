cec4e35484f02e107567f50392df5eadfe2a5378
Mariane <comealongmari@gmail.com>
2025-06-16 16:29:15 -0300

    Aula 1

---
+++ preco.py
@@ -0,0 +1,10 @@
+nome_produto = "Cadeira Infantil"
+unidade = 12.40
+quantidade = 3
+
+soma = unidade * quantidade
+
+print(f"Nome do produto: {nome_produto}")
+print(f"Preço: {unidade}")
+print(f"Quantidade: {quantidade}")
+print(f"Total da compra: {soma}")
---
+++ volume.py
@@ -0,0 +1,7 @@
+comprimento = 12
+largura = 14
+altura = 20
+
+soma = comprimento * largura * altura
+
+print(f"A soma dos valores é: {soma} cm³")
